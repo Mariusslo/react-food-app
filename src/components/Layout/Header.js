dc02d3000074750a3fff6821ee87c22ef0a8832d
Marius Slovikas <41129367+Mariusslo@users.noreply.github.com>
2021-09-26 13:57:47 +0300

    Added Cart to Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import mealsImage from '../../assets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <React.Fragment>
             <header className={styles.header}>
                 <h1>React Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt="Table full of food"/>
